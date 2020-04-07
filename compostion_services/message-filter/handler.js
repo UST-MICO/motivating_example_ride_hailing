d1558e1bf676cab1a1b1a24df7e2f2247f555c55
Christoph Krieger <christoph.krieger@iaas.uni-stuttgart.de>
2020-04-07 15:30:29 +0200

    update function

--- compostion_services/message-filter/handler.js
+++ compostion_services/message-filter/handler.js
@@ -1,9 +1,14 @@
 "use strict"
+// { "cloudEvent": {"id": 1, "time": 11111, "source": "someSource", "data": { "timestamp": 1111, "customerRating": 9, "customerName": "Chris"}}, "patternConfig": { "key": "customerName", "comparison_value": "Chris", "comparison_sign": "equal"}}
+// {"cloudEvent":{"id": 1, "source": "someSource", "data": { "timestamp": 1111, "customerRating": 9, "customerName": "Chris"}}, "patternConfig" :{ "key": "customerName", "comparison_value": "Chris", "comparison_sign": "equal"}}
+module.exports = async (functionInput, callback) => {
 
-module.exports = async (event, callback) => {
-    const cloudEventMessage = JSON.parse(event);
+    functionInput = JSON.parse(functionInput);
+    const cloudEventMessage = functionInput.cloudEvent;
     const eventData = cloudEventMessage.data;
-    const routerConfig = require('./config.json');
+    const routerConfig = functionInput.patternConfig;
+    let result = []
+    //const routerConfig = require('./config.json');
     const comparisons = {
         GREATER: 'greater',
         LESS: 'less',
@@ -13,10 +18,14 @@ module.exports = async (event, callback) => {
         NOTEQUAL: 'notequal'
     }
 
+    //return functionInput;
     // only return messages that match the criteria defined in the routerConfig 
     if (isMatchingCriteria(eventData, routerConfig)) {
-        return event
+        result.push(cloudEventMessage);
+
     }
+
+    return result;
 
     function isMatchingCriteria(data, criteria) {
         switch (criteria.comparison_sign) {
